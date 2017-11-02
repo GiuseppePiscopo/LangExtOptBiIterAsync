1b9b92ca2cbcce7f9543db040ad5c954dce85bf5
GiuseppePiscopo <GiuseppePiscopo@users.noreply.github.com>
2017-11-02 17:50:09 +0100

    feat(program): show issue with Task<Option<>>.BiIterAsync

--- src/LangExtOptBiIterAsync/Program.cs
+++ src/LangExtOptBiIterAsync/Program.cs
@@ -1,14 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using LanguageExt;
+using System;
 using System.Threading.Tasks;
+using static LanguageExt.Prelude;
 
 namespace LangExtOptBiIterAsync
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] _)
         {
+            Console.WriteLine("Press key to start");
+            Console.ReadKey();
+
+            RunIssue().Wait();
+
+            Console.WriteLine("Press key to terminate");
+            Console.ReadKey();
+        }
+
+        static Task RunIssue()
+        {
+            return ValidateAsync();
+        }
+
+        static Task<Unit> ValidateAsync()
+        {
+            Task<Option<string>> textOptTask = Task.Run(() => (Option<string>)None);
+
+            Task<Unit> doActionTask = textOptTask.BiIterAsync(
+                None: () =>
+                    Console.WriteLine("Do Action None"),
+                Some: text =>
+                    Console.WriteLine($"Do Action Some: '{text ?? "<null>"}'"));
+
+            return doActionTask;
         }
     }
 }
